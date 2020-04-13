8ae8a4345e3611cf36adef6ee0a8ce8ecbe9cb60
AAlZarooni <39457920+AAlZarooni@users.noreply.github.com>
2020-04-13 23:55:19 +0400

    Update Restaurant.java

--- FoodTracking/src/sds/Restaurant.java
+++ FoodTracking/src/sds/Restaurant.java
@@ -19,5 +19,85 @@ public class Restaurant {
 		RID=RID2; 
 	}
 	
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private Employee employee;
+
+	/** 
+	* @return the employee
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public Employee getEmployee() {
+		// begin-user-code
+		return employee;
+		// end-user-code
+	}
+
+	/** 
+	* @param employee the employee to set
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public void setEmployee(Employee employee) {
+		// begin-user-code
+		this.employee = employee;
+		// end-user-code
+	}
+
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private RestaurantController restaurantController;
+
+	/** 
+	* @return the restaurantController
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public RestaurantController getRestaurantController() {
+		// begin-user-code
+		return restaurantController;
+		// end-user-code
+	}
+
+	/** 
+	* @param restaurantController the restaurantController to set
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public void setRestaurantController(RestaurantController restaurantController) {
+		// begin-user-code
+		this.restaurantController = restaurantController;
+		// end-user-code
+	}
+
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private Menu menu;
+
+	/** 
+	* @return the menu
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public Menu getMenu() {
+		// begin-user-code
+		return menu;
+		// end-user-code
+	}
+
+	/** 
+	* @param menu the menu to set
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public void setMenu(Menu menu) {
+		// begin-user-code
+		this.menu = menu;
+		// end-user-code
+	}
 		
 }
