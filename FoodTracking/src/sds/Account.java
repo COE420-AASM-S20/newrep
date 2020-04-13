9f1ab06fa38f64ffc7c56a361a8533eaa7d0ad4a
AAlZarooni <39457920+AAlZarooni@users.noreply.github.com>
2020-04-13 23:51:06 +0400

    Update Account.java

--- FoodTracking/src/sds/Account.java
+++ FoodTracking/src/sds/Account.java
@@ -18,4 +18,84 @@ public class Account {
 		else return false; 
 	}
 
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private Administrator administrator;
+
+	/** 
+	* @return the administrator
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public Administrator getAdministrator() {
+		// begin-user-code
+		return administrator;
+		// end-user-code
+	}
+
+	/** 
+	* @param administrator the administrator to set
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public void setAdministrator(Administrator administrator) {
+		// begin-user-code
+		this.administrator = administrator;
+		// end-user-code
+	}
+
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private DBMS dBMS;
+
+	/** 
+	* @return the dBMS
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public DBMS getdBMS() {
+		// begin-user-code
+		return dBMS;
+		// end-user-code
+	}
+
+	/** 
+	* @param dBMS the dBMS to set
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public void setdBMS(DBMS dBMS) {
+		// begin-user-code
+		this.dBMS = dBMS;
+		// end-user-code
+	}
+
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private User user;
+
+	/** 
+	* @return the user
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public User getUser() {
+		// begin-user-code
+		return user;
+		// end-user-code
+	}
+
+	/** 
+	* @param user the user to set
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public void setUser(User user) {
+		// begin-user-code
+		this.user = user;
+		// end-user-code
+	}
 }
