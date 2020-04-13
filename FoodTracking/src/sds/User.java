e70288f30638ca05803ced53452fe5876f3933c0
AAlZarooni <39457920+AAlZarooni@users.noreply.github.com>
2020-04-13 23:56:43 +0400

    Update User.java

--- FoodTracking/src/sds/User.java
+++ FoodTracking/src/sds/User.java
@@ -1,5 +1,40 @@
 package sds;
 
-public interface User {
+/**
+ * 
+ */
 
+/** 
+ * <!-- begin-UML-doc -->
+ * <!-- end-UML-doc -->
+ * @author b00066683
+ * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class User {
+	/** 
+	* <!-- begin-UML-doc -->
+	* <!-- end-UML-doc -->
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	private Account account;
+
+	/** 
+	* @return the account
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public Account getAccount() {
+		// begin-user-code
+		return account;
+		// end-user-code
+	}
+
+	/** 
+	* @param account the account to set
+	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	*/
+	public void setAccount(Account account) {
+		// begin-user-code
+		this.account = account;
+		// end-user-code
+	}
 }
