126e7e03304e0778da85f3d68625748069e43fde
abduhamdan <60571633+abduhamdan@users.noreply.github.com>
2020-04-14 21:55:18 +0400

    Update RController.java

--- FoodTracking/src/sds/RController.java
+++ FoodTracking/src/sds/RController.java
@@ -2,17 +2,30 @@ package sds;
 import java.io.*; 
 import java.util.*; 
 public class RController {
-	ArrayList<int> RDs = new ArrayList<int>();
-	ArrayList<String> RNs = new ArrayList<String>();
-	ArrayList<String> RLs = new ArrayList<String>();
-	ArrayList<int> RCs = new ArrayList<int>();
+	private ArrayList<restaurant> RLALA new ArrayList<restaurant>();  //check Restaurant spelling (capitals)
+	public ArrayList<restaurant> obtaintheListafterSetup()
+	{
+		return RLALA;
+	}
+	//ArrayList<int> RDs = new ArrayList<int>();
+// 	ArrayList<String> RNs = new ArrayList<String>();
+// 	ArrayList<String> RLs = new ArrayList<String>();
+// 	ArrayList<int> RCs = new ArrayList<int>();
 	
 	
 	public static ArrayList<Restaurant> getRList()
 	{
+		read and store( id, name, loc, contact) in variables 
+	RLALA[i] = new Restaurant (id, name, loc, contact);
 		
 	}
 	
-	public void setRList(int id, String name, String loc, int contact)
-	{//reads to file in database}
+	public void setRList(Restaurant W)
+	{W.ID;
+	 //reads to file in database
+	}
 }
+	
+	
+	rname,rid,rl,rc
+		
