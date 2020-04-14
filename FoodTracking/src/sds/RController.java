5e52f9472986fb8e5147fc92563c3f201f8871ab
abduhamdan <60571633+abduhamdan@users.noreply.github.com>
2020-04-14 20:40:36 +0400

    Update RController.java
    Still needs to fill in function of Get and Set (These will get and fill all the arrays from the 4 files)

--- FoodTracking/src/sds/RController.java
+++ FoodTracking/src/sds/RController.java
@@ -2,7 +2,10 @@ package sds;
 import java.io.*; 
 import java.util.*; 
 public class RController {
-	ArrayList<Restaurant> RK = new ArrayList<Restaurant>();
+	ArrayList<int> RDs = new ArrayList<int>();
+	ArrayList<String> RNs = new ArrayList<String>();
+	ArrayList<String> RLs = new ArrayList<String>();
+	ArrayList<int> RCs = new ArrayList<int>();
 	
 	
 	public static ArrayList<Restaurant> getRList()
